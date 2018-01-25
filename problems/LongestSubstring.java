4f0fd221c6cc1224316872f6021d7837d777fcdc
Zhangxingrui <2855892305@qq.com>
2018-01-25 21:02:53 +0800

    最长公共子串问题

---
+++ problems/LongestSubstring.java
@@ -0,0 +1,56 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 最长公共子串
+ * 给定两个字符串，找出两个字符串的最长公共子串
+ * @author 张兴锐
+ *
+ */
+public class LongestSubstring {
+
+	static int current;
+	static int best;
+	static StringBuilder currentStr;
+	static List<String> record = new LinkedList<>();
+	public static void main(String[] args) {
+		String str1 = "abcefg";
+		String str2 = "efgabc";
+		for (int i = 0; i < str1.length(); i++) {
+			for(int j = 0;j<str2.length();j++) {
+				if(str1.charAt(i) == str2.charAt(j)) {
+					//从该点向后扫描
+					current = 1;
+					currentStr = new StringBuilder();
+					currentStr.append(str1.charAt(i));
+					int k1 = i+1;
+					int k2 = j+1;
+					while(k1 < str1.length() && k2 < str2.length() && str1.charAt(k1) == str2.charAt(k2)) {
+						current++;
+						currentStr.append(str1.charAt(k1));
+						k1++;
+						k2++;
+					}
+					if(best < current) {
+						record.clear();
+						record.add(currentStr.toString());
+						best = current;
+					}else if(best == current) {
+						record.add(currentStr.toString());
+					}
+					j = k2-1;
+				}
+			}
+		}
+		//outPut
+		System.out.println("最长公共子序列长度为:"+best);
+		for (String string : record) {
+			System.out.println(string);
+		}
+	}
+	
+
+}
